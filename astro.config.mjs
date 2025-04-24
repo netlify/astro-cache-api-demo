d3887410c90b9f3d7e8818fec09b6a0884e3ef43
Eduardo Bouças <mail@eduardoboucas.com>
2025-04-25 00:46:39 +0100

    Revert to prod adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-// import netlify from '@astrojs/netlify';
-import netlify from "../../withastro/astro/packages/integrations/netlify";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
