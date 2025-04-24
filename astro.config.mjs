73130e6a2ef8feb0a3f3d9bd3751f87fb186ec29
Eduardo Bouças <mail@eduardoboucas.com>
2025-04-24 21:33:33 +0100

    Adjust copy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import netlify from "@astrojs/netlify";
+// import netlify from '@astrojs/netlify';
+import netlify from "../../withastro/astro/packages/integrations/netlify";
 
 // https://astro.build/config
 export default defineConfig({
--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,43 +1,6 @@
 import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
 
-import { getStore } from "@netlify/blobs";
-
-const cache = await caches.open("my-cache");
-
-const getFromCache = async (url: string) => {
-  const cached = await caches.match(url);
-  if (cached) {
-    const data = await cached.json();
-
-    return { cached: true, ...data };
-  }
-
-  const fresh = await fetch(url);
-
-  await cache.put(url, fresh.clone());
-
-  const data = await fresh.json();
-
-  return { cached: false, ...data };
-};
-
-const getFromBlobs = async (url: string, id: string) => {
-  const store = getStore("my-store");
-  const key = `quote-${id}`;
-  const value = await store.get(key, { type: "json" });
-  if (value) {
-    return { cached: true, ...value };
-  }
-
-  const res = await fetch(url);
-  const data = await res.json();
-
-  await store.setJSON(key, data);
-
-  return { cached: false, ...data };
-};
-
 export const server = {
   fetch: defineAction({
     input: z.object({
